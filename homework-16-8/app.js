e9f324c1a5cc7beef8bc33b9d68b51e4b71e9918
Hoang Viet Anh <110024404+HoangVietAnh198@users.noreply.github.com>
2022-08-17 08:16:42 +0700

    Add files via upload

---
+++ homework-16-8/app.js
@@ -0,0 +1,42 @@
+let btn = document.getElementById("btn");
+let box = document.getElementById("box");
+let box2 = document.getElementById("box2");
+let box3 = document.getElementById("box3")
+// ex1
+
+
+// btn.onclick = function() {
+//     if (box.style.display !== "none") {
+//         box.style.display = "none"
+//     } else {
+//         box.style.display = "block"
+//     }
+// }
+
+
+// ex2
+// let color = ['red','gray','pink','yellow','black','blue','aqua','salmon','violet','olive']
+// btn.onclick = function () {
+//    box.style.backgroundColor = color[ramdom(color)];
+// }
+// function ramdom (array) {
+//     return Math.floor(Math.random()*10);
+// }
+
+// ex3
+    // btn.onclick = function () {
+    //     if (box.style.backgroundColor = "red"){
+    //         box.style.backgroundColor = "blue";
+    //         box2.style.backgroundColor = "red";
+    //     } else if (box.style.backgroundColor = "blue"){
+    //         box.style.backgroundColor = "red";
+    //         box2.style.backgroundColor = "blue";
+    //     }
+    // }
+
+// ex4
+// let a = 10
+// btn.onclick = function() {
+//     a = a + 1
+//     box3.style.fontSize = `${a}px`
+// } 
